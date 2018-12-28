7f5c7ef1843dd4bc983b35b39bbe4a6a061701af
Oliver <s154023@student.dtu.dk>
2018-12-28 19:08:19 +0100

    Add loading state to login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,37 +1,65 @@
 import CustomButton from '../components/CustomButton';
 import React from 'react'
 import { logIn, signUp } from 'instant-messaging'
-import { StyleSheet, TextInput, View, KeyboardAvoidingView, TouchableOpacity, Text } from 'react-native'
+import { StyleSheet, TextInput, View, KeyboardAvoidingView, ActivityIndicator } from 'react-native'
 
 
 export default class Login extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			loading: false,
+		}
 	}
 
 	render() {
 		return (
 			<KeyboardAvoidingView style={styles.container} behavior="padding">
 				<TextInput style={styles.textInput} placeholder="Enter username" onChangeText={username => this._username = username}/>
-				<View style={styles.actions}>
-					<CustomButton text='Login' type='primary' style={styles.button} onPress={() => this._login()}/>
-					<CustomButton text='Sign up' type='secondary' style={styles.button} onPress={() => this._signUp()}/>
-				</View>
+				{this.state.loading ?
+					<View>
+						<ActivityIndicator/>
+					</View>
+				:
+					<View style={styles.actions}>
+						<CustomButton text='Login' type='primary' style={styles.button} onPress={() => this._login()}/>
+						<CustomButton text='Sign up' type='secondary' style={styles.button} onPress={() => this._signUp()}/>
+					</View>
+				}
 			</KeyboardAvoidingView>
 		);
 	}
 
 	_login() {
+		this._enableLoadingState();
 		logIn(this._username)
 			.then(() => this.props.navigation.replace({routeName: 'Chats'}))
-			.catch(error => alert(error));
+			.catch(error => alert(error))
+			.finally(this._disableLoadingState.bind(this));
+
 	}
 
 	_signUp() {
+		this._enableLoadingState();
 		signUp(this._username)
 			.then(() => this.props.navigation.replace({routeName: 'Chats'}))
-			.catch(error => alert(error));
+			.catch(error => alert(error))
+			.finally(this._disableLoadingState.bind(this));
+
+	}
+
+	_enableLoadingState() {
+		this.setState({
+			loading: true,
+		})
+	}
+
+	_disableLoadingState() {
+		this.setState({
+			loading: false,
+		})
 	}
 }
 
